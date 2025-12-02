7fdfbb45b07c905937d5bfeb7a8066e1613aca2a
brunobb9 <bruno.benny77@outlook.com>
2025-12-02 15:44:45 +0000

    Movie posters now open Wikipedia and other QoL.

--- movie-app/src/Header.jsx
+++ movie-app/src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="headerGlobal">
       <div className="headerContainer">
-        <h1 className="vhs" data-text="Get To Da Movie!">
+        <h1 className="vhs" data-text="Get To Da Movie!" onClick={() => window.location.reload()} style={{cursor:"pointer"}}>
           Get To Da Movie!
         </h1>
         <h3 className="header2">Search for your favorite movies here</h3>
--- movie-app/src/Search.jsx
+++ movie-app/src/Search.jsx
@@ -1,22 +1,29 @@
 import { useState } from "react";
 
+
+
 export default function Search () {
 
 const [type, setType] = useState("")
 const [results, setResults] = useState([]);
 const [query, setQuery] = useState("");
+const [searched, setSearched] = useState(false);
 
 const handleSearch = async () => {
 const apiKey="98594a4eda50697575b42d75dc200b7f";
 const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${query}`);
 const data = await response.json();
 
+setSearched(true);
+
 console.log(data.results);
 setResults(data.results);
-
-
-
 }
+
+const openWikipedia = (title) => {
+  const url=`https://en.wikipedia.org/w/index.php?search=${encodeURIComponent(title)}`
+  window.open(url, "_blank");
+};
 
 return (
   <>
@@ -35,15 +42,32 @@ return (
 />
 
       <button onClick={handleSearch}>Search</button>
+
     </div>
 
-    {results.length === 0 && (
-        <div className="introText">
-          <h2 style={{marginTop:"6rem", fontSize:"2.5rem" }}>Here you can find all your favorite films.</h2><h2>Now Get To Da Movie!</h2>
-        </div>
-      )}
+
+      {!searched && (
+  <div className="introText">
+    <h2 className="h2intro" style={{ marginTop: "6rem" }}>
+      Here you can find all your favorite films.
+    </h2>
+    <h2>Now Get To Da Movie!</h2>
+  </div>
+)}
+
+{searched && results.length === 0 && (
+  <div className="introText">
+    <h2 className="h2intro" style={{ marginTop: "6rem" }}>
+      Sorry, we couldn't find your film!
+    </h2>
+  </div>
+)}
+
+       
+       
 
 <div className="results" style={{ paddingTop: results.length ? "1rem" : 0 }}>
+
   {results.map((movie) => (
     <div key={movie.id} className="movieCard">
 
@@ -52,6 +76,8 @@ return (
           <img
             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
             alt={movie.title}
+            onClick={() => openWikipedia(movie.title)}
+            style = {{cursor:"pointer"}}
           />
         )}
       </div>
